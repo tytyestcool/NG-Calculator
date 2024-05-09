d16d261e1862ee57a3b199e2a59cbd11acbce180
Krajewski <trykraj@gmail.com>
2024-05-09 21:13:42 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+const root = resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/NG-Calculator/"
+  base:"/NG-Calculator/",
+  build:{
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        calculator: resolve(root, 'index.html')
+      }
+    }
+  }
 })
